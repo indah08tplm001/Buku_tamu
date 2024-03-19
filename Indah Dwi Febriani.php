56f8c6d0f1efeea48e88120821e694aa0ca6e2a2
indah08tplm001 <162332572+indah08tplm001@users.noreply.github.com>
2024-03-20 05:59:13 +0700

    Add files via upload

---
+++ Indah Dwi Febriani.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Indah dwi febriani</title>
+    </head>
+        <body>
+        <br><big><pre><font face="Arial" size="+3" color=red><b>                      BIODATA </b></pre></font></big>
+        <?php
+            print"<pre>";
+            print"<font size = 5 color = 'blue'>           Nama    : Indah Dwi Febriani </font><br>";
+            print"<font size = 5 color = 'blue'>           Email   : Indahdw@gmail.com </font><br>";
+            print"<font size = 5 color = 'blue'>           Jurusan : Teknik Informatika </font><br>";
+            print"<font size = 5 color = 'blue'>           Hoby    : Traveling </font><br><br><br>";
+
+            print"<font size = 2 color = 'black'>          Kesan pertama kali belajar PHP itu lebih mengasyikan dan lebih mudah daripada ketika awal belajar bahasa c++ ataupun java </font><br>";
+        ?>
+        </body>
+</html>
